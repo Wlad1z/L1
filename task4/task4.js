0cb399684585928cad74a402bc49364ed4ecc40c
Wlad1z <113522178+Wlad1z@users.noreply.github.com>
2023-08-22 11:19:13 +0300

    add 4

--- task4/task4.js
+++ task4/task4.js
@@ -1,4 +1,5 @@
 import {getDeclension} from "../module/functionTask4.js"
+import {createSimpleElement} from "../module/createSimpleElement.js"
 
 const number = 12454;
 const words = ["сообщение", "сообщения", "сообщений"];
