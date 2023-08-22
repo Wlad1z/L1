61e05bd8d4b1f005b6aed446a5fb6bbb0f9aeea4
Wlad1z <113522178+Wlad1z@users.noreply.github.com>
2023-08-22 13:25:50 +0300

    Update task24.js

--- task24/task24.js
+++ task24/task24.js
@@ -1,6 +1,6 @@
 
 const del = document.querySelector('.remove');
-fetch('http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true')
+fetch('https://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true')
     .then(response => response.json())
     .then(data => {
         
