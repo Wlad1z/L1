70048c0c199519d83158837d072a93c2e1c2497b
Wlad1z <113522178+Wlad1z@users.noreply.github.com>
2023-08-25 23:47:29 +0300

    Update task24.js

--- task24/task24.js
+++ task24/task24.js
@@ -13,7 +13,7 @@ async function jsonp(url) {
 
 //информируем о загрузке данных
 const del = document.querySelector('.remove');
-jsonp('http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true')
+jsonp('https://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true')
     .then(data => {
         
         del.remove();
