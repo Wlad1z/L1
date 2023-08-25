235471dda2e64560da26d0624b01e9d1ddb06e58
Wlad1z <113522178+Wlad1z@users.noreply.github.com>
2023-08-25 23:39:58 +0300

    Update task24.js

--- task24/task24.js
+++ task24/task24.js
@@ -1,8 +1,19 @@
+//отправляем асинхронный запрос к указанной ссылке, для получения данных
+async function jsonp(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Произошла ошибка при получении данных:', error);
+        throw error;
+    }
+}
+
 
 //информируем о загрузке данных
 const del = document.querySelector('.remove');
-fetch('https://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true')
-    .then(response => response.json())
+jsonp('http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true')
     .then(data => {
         
         del.remove();
